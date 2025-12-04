465ce78344e2092246fdb801158c45916724339a
salvatoremosca <49988614+salvatoremosca@users.noreply.github.com>
2025-12-04 10:57:54 +0100

    Delete tasks directory

--- tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(UserAdmin):
-    list_display = ("username", "email", "is_active", "date_joined")
-    list_filter = ("is_staff", "is_active")
